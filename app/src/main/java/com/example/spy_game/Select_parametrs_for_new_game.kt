5c4a6692eca01ae2d50d2ffc7f361129f4b4d719
solntsev.d <32107379+Demoncho@users.noreply.github.com>
2019-09-15 16:21:17 +0300

    add text and seekBar for players and time

--- app/src/main/java/com/example/spy_game/Select_parametrs_for_new_game.kt
+++ app/src/main/java/com/example/spy_game/Select_parametrs_for_new_game.kt
@@ -2,11 +2,54 @@ package com.example.spy_game
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.TextView
 
 class Select_parametrs_for_new_game : AppCompatActivity() {
+
+    lateinit var slider_players: SeekBar
+    lateinit var value_players: TextView
+
+    lateinit var slider_time: SeekBar
+    lateinit var value_time: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_parametrs_for_new_game)
+
+        slider_players = findViewById(R.id.seekBar_count_of_players) as SeekBar
+        value_players = findViewById(R.id.count_of_players_text) as TextView
+
+        slider_players.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                value_players.text = "Количество игроков: " + (progress.toInt() + 4).toString()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                value_players.text = "Количество игроков: " + (seekBar.progress.toInt() + 4).toString()
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                value_players.text = "Количество игроков: " + (seekBar.progress.toInt() + 4).toString()
+            }
+        })
+
+        slider_time = findViewById(R.id.seekBar_count_of_time) as SeekBar
+        value_time = findViewById(R.id.count_of_time_text) as TextView
+
+        slider_time.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                value_time.text = "Время(мин): " + (progress.toInt() + 4).toString()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                value_time.text = "Время(мин): " + (seekBar.progress.toInt() + 4).toString()
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                value_time.text = "Время(мин): " + (seekBar.progress.toInt() + 4).toString()
+            }
+        })
+
     }
 }
