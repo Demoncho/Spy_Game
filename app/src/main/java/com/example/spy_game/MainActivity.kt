f7086500b7e3fcaf40e03be3243dd26b952b82d7
solntsev.d <32107379+Demoncho@users.noreply.github.com>
2019-09-15 15:07:01 +0300

    add second_activity

--- app/src/main/java/com/example/spy_game/MainActivity.kt
+++ app/src/main/java/com/example/spy_game/MainActivity.kt
@@ -1,9 +1,17 @@
 package com.example.spy_game
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+
 
 class MainActivity : AppCompatActivity() {
+
+    fun start_new_game(view: View){
+        val select_param = Intent(this, Select_parametrs_for_new_game::class.java)
+        startActivity(select_param)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/example/spy_game/Select_parametrs_for_new_game.kt
@@ -0,0 +1,12 @@
+package com.example.spy_game
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Select_parametrs_for_new_game : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_parametrs_for_new_game)
+    }
+}
